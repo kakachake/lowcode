0793f9e4855a93218c0b42ffe36b77e84058525b
卡卡查克 <282281884@qq.com>
2023-04-02 18:28:28 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // 箭头函数只有一个参数时，是否省略括号
+  arrowParens: 'avoid',
+  // 括号内不要出现空格
+  bracketSpacing: true,
+  // 行结束符
+  endOfLine: 'lf',
+  // jsx 标签闭合位置
+  jsxBracketSameLine: false,
+  // 行宽
+  printWidth: 80,
+  // 分号
+  semi: true,
+  // 单引号
+  singleQuote: true,
+  // 缩进
+  tabWidth: 2,
+  // 使用tab缩进
+  useTabs: false,
+  parser: 'typescript',
+};
